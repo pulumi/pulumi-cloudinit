92493ff688ebd3ee77dd692ea8fe37d3d33822f8
Guinevere Saenger <guinevere@pulumi.com>
2024-08-23 10:07:22 -0700

    Add installation docs (#390)
    Part of https://github.com/pulumi/home/issues/3598.

--- provider/resources.go
+++ provider/resources.go
@@ -15,6 +15,7 @@
 package cloudinit
 
 import (
+	"bytes"
 	"fmt"
 	"path"
 
@@ -54,6 +55,7 @@ func Provider() tfbridge.ProviderInfo {
 		Repository:   "https://github.com/pulumi/pulumi-cloudinit",
 		GitHubOrg:    "hashicorp",
 		MetadataInfo: tfbridge.NewProviderMetadata(metadata),
+		DocRules:     &tfbridge.DocRuleInfo{EditRules: docEditRules},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
@@ -119,4 +121,22 @@ func Provider() tfbridge.ProviderInfo {
 	prov.SetAutonaming(255, "-")
 
 	return prov
+}
+
+func docEditRules(defaults []tfbridge.DocsEdit) []tfbridge.DocsEdit {
+	return append(
+		defaults,
+		removeDataSource,
+	)
+}
+
+// Tightens up content explanation
+var dataSource = []byte("data source, previously available as the `template_cloudinit_config` " +
+	"resource in the template provider,")
+var removeDataSource = tfbridge.DocsEdit{
+	Path: "index.md",
+	Edit: func(_ string, content []byte) ([]byte, error) {
+		content = bytes.ReplaceAll(content, dataSource, []byte("function"))
+		return content, nil
+	},
 }
